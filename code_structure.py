5c3a716c3d074b3d3607e126db09980d566ec9d8
Sara Piccini <92572907+SaraPiccini@users.noreply.github.com>
2022-08-07 12:54:53 +0200

    Create code_structure.py

---
+++ code_structure.py
@@ -0,0 +1,57 @@
+# STATEMENT
+s = "python"
+
+#test condizionale
+if expression:
+  suite
+elif expression:
+  suite
+else expression:
+  suite
+
+#loop
+while expression:
+  suite
+else:
+  suite
+#ex:
+x = 0
+while x < 3:
+  print(x)
+  x += 1 
+# 0 1 2
+
+while True: #loop infinito
+  x = input ("Inserire una stringa")
+  if x == 'stop':
+    break #per uscire dal loop infinito
+ptint(x)
+#continue per rieseguire il loop
+
+#iterazioni
+myList = [1,2,3]
+for i in myList:
+  print(i)
+#1,2,3
+
+myDict = {'a': 1, 'b': 2, 'c': 3}
+for i in myDict:
+  print (i)
+#a,b,c
+for i in myDict.values():
+  print(i)
+#1,2,3
+for i in myDict.items():
+  print(i)
+#a:1,b:2,c:3
+
+# range(start,stop,step) function
+for i in range (10,16,2):
+  print(i)
+#10,12,14
+
+# LIST COMPREHENSION
+,,,
+
+
+  
