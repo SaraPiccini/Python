bb3e9e919d8b80d7b22a102920913e23dccf2b06
Sara Piccini <92572907+SaraPiccini@users.noreply.github.com>
2022-08-07 11:16:31 +0200

    Create data_type.py

---
+++ data_type.py
@@ -0,0 +1,7 @@
+myList = [1,2,3,4]
+myList = list()
+myList.insert (2,8) #1,2,8,3,4
+myList.append(6) #1,2,3,4,6
+del myList[1] #1,3,4
+2 in myList #True
+myList2 = myList #una modifica in una lista si riflette anhe nell'altra
