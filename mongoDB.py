a7f7a456252bd2401e010577d5b90cebf927a722
Sara Piccini <92572907+SaraPiccini@users.noreply.github.com>
2022-08-16 10:39:12 +0200

    Create mongoDB.py

---
+++ mongoDB.py
@@ -0,0 +1,18 @@
+# rappresentazione oggeto in JSON
+{
+  "nome": "Mario",
+  "cognome":"Rossi",
+  "età":30
+  "computer":{ 
+    "comp1":"Asus",
+    "comp2":"Apple"
+  } #oggetto nidificato nell'oggetto
+}
+
+# array in JSON
+{
+  "nome": "Mario",
+  "computer": ["Asus", "Apple"]
+}
+
+  
